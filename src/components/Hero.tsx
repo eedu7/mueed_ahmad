4ba8a07cc0f930102fba073dc791c42f767bdc85
eedu7 <mueedahmad067@gmail.com>
2025-09-09 06:41:48 +0500

    feat(hero): add Hero component with intro text, image, and CTA button

---
+++ src/components/Hero.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+
+export const Hero = () => {
+	return (
+		<div className="grid h-screen place-items-center">
+			<div>
+				<div className="flex flex-col items-center justify-center gap-y-3 font-light capitalize">
+					<div className="flex items-center justify-center">
+						<Image
+							src="/person.png"
+							alt="Person Image"
+							width={400}
+							height={400}
+							priority
+							className="h-auto w-[150px]"
+						/>
+						<span className="absolute text-3xl font-semibold text-white">Hi</span>
+					</div>
+					<h1 className="text-center text-3xl font-bold tracking-wider text-gray-500">
+						My Name is Mueed Ahmad &
+					</h1>
+					<p className="text-lg tracking-wider text-gray-700">I like animations 😊</p>
+				</div>
+				<div>
+					<a href="#">Icon</a>
+				</div>
+				<a
+					href="#"
+					className="mx-auto mt-7 block w-max rounded-lg bg-red-400 px-3 py-1 font-light tracking-wider text-white capitalize transition-colors hover:bg-red-500"
+				>
+					Talk to me
+				</a>
+			</div>
+		</div>
+	);
+};
